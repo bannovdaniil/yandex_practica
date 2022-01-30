a15fb1a351fff3c864a289dab728c6dcb29a317e
bannovdaniil <baddan@mail.ru>
2022-01-30 15:21:12 +0300

    Sber node #2 on ArrayList + Pair + check Doubles.. work solution

--- src/ru/yandex/cars/Sber.java
+++ src/ru/yandex/cars/Sber.java
@@ -1,123 +1,106 @@
 package ru.yandex.cars;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 
 public class Sber {
-    static class Node {
-        String name;
-        ArrayList<Node> nodes;
 
-        public Node(String name) {
-            this.name = name;
-            this.nodes = new ArrayList<>();
-        }
+    static class Pair {
+        String first;
+        String second;
 
-        public void addChild(String nodeName) {
-            if (findChild(nodeName) == null) {
-                this.nodes.add(new Node(nodeName));
-            }
-        }
-
-        public Node findChild(String nodeName) {
-            for (Node node : nodes) {
-                if (node.name.equals(nodeName)) {
-                    return node;
-                }
-            }
-            return null;
-        }
-
-        public Node findNode(Node nodeRoot, String nodeName) {
-            Node find = null;
-            if(nodeRoot.name.equals(nodeName)){
-                return nodeRoot;
-            }
-            for (Node node : nodeRoot.nodes) {
-                if (node.name.equals(nodeName)) {
-                    return node;
-                } else {
-                    Node temp = findNode(node, nodeName);
-                    if (temp != null) {
-                        return temp;
-                    }
-                }
-            }
-            return find;
-        }
-
-        @Override
-        public String toString() {
-            String result = name + " => ";
-            for (var node : nodes) {
-                result += node.name + ", ";
-            }
-            return result;
+        public Pair(String first, String second) {
+            this.first = first;
+            this.second = second;
         }
     }
 
     public static int getResult(List<String> deal) {
-        HashSet<String> roots = new HashSet<>();
-        Node root = null;
-        Node node = null;
+// собираем пары
+        ArrayList<Pair> links = new ArrayList<>();
         for (var first : deal) {
-            node = root;
             if (first.contains("-")) {
+                ArrayList<String> path = new ArrayList<>();
                 String[] company = first.split("-");
-                roots.add(company[0]);
-                if (node == null) {
-                    node = new Node(company[0]);
-                    if (root == null) {
-                        root = node;
-                    }
-                } else {
-                    node = node.findNode(root, company[0]);
-                }
                 for (var second : company[1].split("")) {
-                    if (node == null) {
-                        node = root.findNode(root, second);
-                        node.addChild(company[0]);
-                    }
-                    node.addChild(second);
+                    links.add(new Pair(company[0], second));
                 }
             } else {
-                roots.add(first);
+                links.add(new Pair(first, ""));
             }
         }
-
-
+//        for (var p : links) {
+//            System.out.println(p.first + " => " + p.second);
+//        }
         int countLinks = 0;
-        node = root;
-        for (var top : roots) {
-            countLinks += getPath(node, top, 0);
-            node = node.findNode(root, top);
+        for (int i = 0; i < links.size(); i++) {
+            String first = links.get(i).first;
+            ArrayList<Integer> path = new ArrayList<>();
+            path.add(i);
+            ArrayList<String> phrases = new ArrayList<>();
+            countLinks += getPath(path, i, first, links.get(i), links, first, phrases);
+            links.set(i, new Pair(first, ""));
         }
-        System.out.println(countLinks);
+        //    System.out.print(countLinks + "- ");
         return countLinks;
     }
 
-    private static int getPath(Node node, String name, int count) {
+    private static int getPath(ArrayList<Integer> index, int start, String first,
+                               Pair pair, ArrayList<Pair> links, String path, ArrayList<String> phrases) {
         int inner = 0;
-        if (node == null)
-            return inner;
- //       System.out.println(node);
-        for (var child : node.nodes) {
-            if (child.name.equals(name)) {
-                inner++;
+        for (int j = start; j < links.size(); j++) {
+            if (index.contains(j)) continue;
+            if (links.get(j).first.equals(pair.second)) {
+                //
+                String phrase = path + links.get(j).first + links.get(j).second;
+                if (links.get(j).second.equals(first)) {
+                    //      index.add(j);
+                    //     System.out.println(j + "=>" + path + links.get(j).first + links.get(j).second);
+                    //             System.out.println(index);
+                    // удаляем дубляж маршрутов, при этом, не трогаем крайние
+                    for (int i = 1; i < phrase.length() - 1; i++) {
+                        char ch = phrase.charAt(i);
+                        int lastPos = phrase.lastIndexOf(ch);
+                        if (phrase.indexOf(ch) != lastPos) {
+                            // удаляем последний дубль
+                            phrase = phrase.substring(0, lastPos)
+                                    + phrase.substring(lastPos + 1);
+                            i = 1;
+                        }
+                    }
+                    if (!phrases.contains(phrase)) {
+                        phrases.add(phrase);
+                        inner++;
+                    }
+                    // break;
+                } else {
+                    if (phrase.length() < index.size()) {
+                        ArrayList<Integer> cache = new ArrayList<>();
+                        for (int i = 0; i < phrase.length() - 1; i++) {
+                            cache.add(index.get(i));
+                        }
+                        index = cache;
+                    }
+                    index.add(j);
+                    //   System.out.println(temp);
+                    inner += getPath(index, start, first, links.get(j),
+                            links, path + links.get(j).first, phrases);
+                }
             }
-            inner += getPath(child, name, count++);
         }
         return inner;
     }
 
     public static void main(String[] args) {
-               System.out.println(1 == getResult(List.of("a-b", "b-c", "c-a")));
-             System.out.println(1 == getResult(List.of("a-b", "b-c", "c-ad", "d")));
-           System.out.println(0 == getResult(List.of("a-b", "b", "c-b")));
-        System.out.println(2 == getResult(List.of("a-b", "b-cd", "c-a", "d-c")));
-
+//       System.out.println(1 == getResult(List.of("a-b", "b-c", "c-a")));
+        //     System.out.println(1 == getResult(List.of("a-b", "b-c", "c-ad", "d")));
+        //    System.out.println(0 == getResult(List.of("a-b", "b", "c-b")));
+        //       System.out.println(2 == getResult(List.of("a-b", "b-cd", "c-a", "d-c")));
+        //     System.out.println(3 == getResult(List.of("a-b", "b-ce", "c-d", "d-e", "e-fc", "f-a")));
         System.out.println(5 == getResult(List.of("a-b", "b-c", "c-da", "d-ea", "e-fa", "f-ac")));
-
+// abcde
+// abcdefa
+// abcdecd
+// abefa
     }
 }
